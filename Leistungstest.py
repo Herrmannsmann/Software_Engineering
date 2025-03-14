220a4a6429e2cb83833200bdd2b45241250e7953
Herrmannsmann <mth.cassan@mci4me.at>
2025-03-14 13:59:40 +0100

    Update Leistungstest.py
    Test

--- Leistungstest.py
+++ Leistungstest.py
@@ -1,1 +1,23 @@
-#Python Code hier
+#########Code Moritz#############################
+leistungstests = []            #leere List erstellen
+
+first_experiment_id = input("Zahl, wo Leistungstest beginnen soll: ")         #first_experiment_id per user input definieren
+
+try:
+    first_experiment_id : int = int(first_experiment_id)                     #Überprüfen ob es sich bei first_experiment_id um einen intatcher handelt
+
+    for i in range(10):                                                    #Zehn Leistungstests erstellen
+        test = {
+            "Versuchsleiter": "Moritz",
+            "Erstelldatum": "14.03.2025",
+            "id_nummer": i+int(first_experiment_id)
+        }
+        leistungstests.append(test)                       #Leistungstests in Liste hinzufügen
+
+    for test in leistungstests[:5]:                     #ersten 5 Leistungstests ausgeben
+        print(test)
+            
+    print("Experimente mit gerader id:",sum(1 for test in leistungstests if test["id_nummer"] % 2 == 0))       #Ausgeben, Anzahl Leistungstests mit gerader id
+
+except ValueError:                                                 #Fehler ausgeben, falls first_experiment_id kein intatcher ist, um Absturz zu verhinern
+    print("Fehler: Bitte gib eine gültige ganze Zahl ein.")
