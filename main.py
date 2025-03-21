9a65c2cef84af84c712d59c150dd5282b45b2eff
MoCa00 <mth.cassan@mci4me.at>
2025-03-21 13:52:22 +0100

    Update main.py
    Vorher nicht vollständig

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from my_functions import estimate_max_hr
+from my_functions import estimate_max_hr, build_person, build_experiment
 
 estimate_max_hr(age_years = 25, sex = "male")
+build_person("Moritz", "Bestermann", "männlich", 25)
+build_experiment("Experiment Test", "21.03.2025", "Samuel", "SWE")
 
 
 if __name__ == "__main__":
