87b21c19a4ea4edb2b1f0c51529f0e690df2cc6d
MoCa00 <mth.cassan@mci4me.at>
2025-03-21 13:53:53 +0100

    Update main.py
    erneute verbesserung

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from my_functions import estimate_max_hr, build_person, build_experiment
-
-estimate_max_hr(age_years = 25, sex = "male")
-build_person("Moritz", "Bestermann", "männlich", 25)
-build_experiment("Experiment Test", "21.03.2025", "Samuel", "SWE")
-
-
 if __name__ == "__main__":
+    from my_functions import estimate_max_hr, build_person, build_experiment
+
+    estimate_max_hr(age_years = 25, sex = "male")
+    build_person("Moritz", "Bestermann", "männlich", 25)
+    build_experiment("Experiment Test", "21.03.2025", "Samuel", "SWE")
+
     print("Hat funktioniert") 
